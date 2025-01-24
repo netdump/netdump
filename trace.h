78a32803bd5c3ff23140d155c08602d8cc6adb00
nothing <nothing163mail@163.com>
2025-01-25 00:17:32 +0800

    [fix] Fix trace_log macro

--- trace.h
+++ trace.h
@@ -18,8 +18,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define trace_log(fmt, ...)                                                     \
-    printf("[%s][%s:%d]"fmt, __TIME__, __func__, __LINE__, ##__VA_ARGS__);      \
+#define trace_log(fmt, ...)                                                                                                             \
+    do {                                                                                                                                \
+        printf("[%s][%s:%d]"fmt, __TIME__, __func__, __LINE__, ##__VA_ARGS__);                                                          \
+    } while (0);                                                                                                                        \
 
 
 #endif
