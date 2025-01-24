723596433fd027ae7816977dbcecd8ca5b0e09fb
nothing <nothing163mail@163.com>
2025-01-24 14:11:07 +0800

    [update] Add print related macros

---
+++ trace.h
@@ -0,0 +1,25 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#ifndef __TRACE_H__
+#define __TRACE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define trace_log(fmt, ...)                                                     \
+    printf("[%s][%s:%d]"fmt, __TIME__, __func__, __LINE__, ##__VA_ARGS__);      \
+
+
+#endif
