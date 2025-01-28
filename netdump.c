8b610364f680abf0f18a44d8c5df0035c8c847a3
nothing <nothing163mail@163.com>
2025-01-29 01:15:05 +0800

    [update] add sigact handle in netdump.c

--- netdump.c
+++ netdump.c
@@ -17,9 +17,12 @@
 
 #include "netdump.h"
 #include "display.h"
+#include "sigact.h"
 
 
 int main(int argc, char ** argv) {
+
+    sigact_register_signal_handle();
 
     display_startup_TUI_showcase();
     display_exit_TUI_showcase();
