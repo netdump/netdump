a4e62a78485ffae50f376364f361643d3f6303bb
nothing <nothing163mail@163.com>
2025-01-29 17:25:38 +0800

    [add] add common.c file

---
+++ common.c
@@ -0,0 +1,26 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "trace.h"
+#include "common.h"
+
+
+/**
+ * @brief Global process number
+ */
+uint32_t GCOREID = 0;
+
+
