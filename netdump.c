c02d2a75d8ab008b76c0741ac7e0730ac8c38669
nothing <nothing163mail@163.com>
2025-01-29 22:07:54 +0800

    [add] TC(Called ...) & Return macro

--- netdump.c
+++ netdump.c
@@ -24,6 +24,8 @@
 
 int main(int argc, char ** argv) {
 
+    TC("Called { %s (%d, %p)", __func__, argc, argv);
+
     TRACE_STARTUP();
 
     sigact_register_signal_handle();
@@ -31,5 +33,5 @@ int main(int argc, char ** argv) {
     display_startup_TUI_showcase();
     display_exit_TUI_showcase();
 
-    return 0;
+    RInt(0);
 }
