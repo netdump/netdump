8f4891b7d0e07a8087b2eaf8158938ce4f0ed04a
nothing <nothing163mail@163.com>
2025-01-29 17:27:18 +0800

    [update] add version macro; add var type translate; add return value; modify GCOREID

--- common.h
+++ common.h
@@ -15,26 +15,80 @@
 #define __COMMON_H__
 
 
+/**
+ * @brief Major version number
+ */
+#define NETDUMP_MAJOR_VERSION           0x0001U
+#define MAJOR_V                         NETDUMP_MAJOR_VERSION
+/**
+ * @brief Subversion number
+ */
+#define NETDUMP_SUB_VERSION             0x0001U
+#define SUB_V                           NETDUMP_SUB_VERSION
+
+
+/** C99：编译时使用 -std=c99，编译器会定义 __STDC_VERSION__ 宏为 199901L */
+/** C89：编译时使用 -std=c89，编译器会定义 __STDC_VERSION__ 宏为 199409L */
+/** C99 */
+#if __STDC_VERSION__ >= 199901L
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#else 
+
+#define true                1
+#define false               0
+
+typedef char                int8_t;
+typedef unsigned char       uint8_t;
+typedef unsigned char       bool;
+
+typedef char *              int8_t *;
+
+typedef short               int16_t;
+typedef unsigned short      uint16_t;
+
+typedef int                 int32_t;
+typedef unsigned int        uint32_t;
+
+typedef long long           int64_t;
+typedef unsigned long long  uint64_t;
+
+#endif
+
+
+
+/**
+ * @brief Error Code
+ */
+enum {
+    ND_OK,
+    ND_ERR,
+};
+
+
 /** DISPLAY PROCESS ID */
-#define GCOREID_DP      0
+#define GCOREID_DP      0U
 /** CAPTURE PROCESS ID */
-#define GCOREID_CP      1
+#define GCOREID_CP      1U
 /** ANALYSIS PROCESS ID */
-#define GCOREID_AA      2
+#define GCOREID_AA      2U
 
 
 /**
  * @brief Global process number
  */
-unsigned int GCOREID = 0;
+extern uint32_t GCOREID;
 
 /**
  * @brief Get the GCOREID of the current process
  */
-__extern_always_inline unsigned int lcore_id(void) {
+__extern_always_inline uint32_t lcore_id(void) {
     
     return GCOREID;
 }
+
 
 
 #endif 
