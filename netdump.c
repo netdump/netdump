04142f3db5c57be6d080c890d226bd6249ee0080
nothing <nothing163mail@163.com>
2025-01-30 01:14:52 +0800

    [mod] add trace_destruction and sub display_exit_TUI_showcase

--- netdump.c
+++ netdump.c
@@ -33,7 +33,8 @@ int main(int argc, char ** argv) {
     sigact_register_signal_handle();
 
     display_startup_TUI_showcase();
-    display_exit_TUI_showcase();
+    
+    TRACE_DESTRUCTION();
 
-    RInt(0);
+    return 0;;
 }
