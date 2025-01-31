11008892300a1fc9d3cd0fcddcf7dd5cf43b57b3
nothing <nothing163mail@163.com>
2025-01-31 18:51:57 +0800

    [add] add function api

--- msgcomm.c
+++ msgcomm.c
@@ -10,3 +10,410 @@
  * 
  * 
  */
+
+
+#include "msgcomm.h"
+#include "common.h"
+#include "trace.h"
+
+
+/**
+ * @brief 
+ *  Global variables for message communication, 
+ *  used for initialization and release
+ * @memberof [0]    0TO1
+ * @memberof [1]    1TO0
+ */
+msgcomm_t msgcomm[] = {
+    {
+        .comm.ring = NULL,
+        .comm._ring = NULL,
+        .comm.baseaddr = MSGCOMM_BASE_ADDR_RING0TO1,
+        .comm._baseaddr = MSGCOMM_BASE_ADDR__RING0TO1,
+        .comm.count = MSGCOMM_BLOCK_NUMBERS,
+        .comm.name = MSGCOMM_RING_FNAME_0TO1,
+        .comm._name = MSGCOMM__RING_FNAME_0TO1,
+
+        .msg.memory = NULL,
+        .msg.baseaddr = MSGCOMM_BASE_ADDR_MEM0TO1,
+        .msg.dir = MSGCOMM_DIR_0TO1,
+        .msg.memspace = MSGCOMM_MEMORY_SPACE,
+        .msg.name = MSGCOMM_MEM_FNAME_0TO1
+    }, 
+    {
+        .comm.ring = NULL,
+        .comm._ring = NULL,
+        .comm.baseaddr = MSGCOMM_BASE_ADDR_RING1TO0,
+        .comm._baseaddr = MSGCOMM_BASE_ADDR__RING1TO0,
+        .comm.count = MSGCOMM_BLOCK_NUMBERS,
+        .comm.name = MSGCOMM_RING_FNAME_1TO0,
+        .comm._name = MSGCOMM__RING_FNAME_1TO0,
+
+        .msg.memory = NULL,
+        .msg.baseaddr = MSGCOMM_BASE_ADDR_MEM1TO0,
+        .msg.dir = MSGCOMM_DIR_1TO0,
+        .msg.memspace = MSGCOMM_MEMORY_SPACE,
+        .msg.name = MSGCOMM_MEM_FNAME_1TO0
+    }
+
+};
+
+
+/**
+ * @brief 
+ *  Initialize the msgcomm_t structure
+ * @param vmsgcomm
+ *  msgcomm_t structure pointer
+ * @return
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+static int msgcomm_init_msgcomm(msgcomm_t * vmsgcomm) {
+
+    TC("Called { %s(%p)", __func__, vmsgcomm);
+
+    if (unlikely((nd_check_fpath(vmsgcomm->comm.name)) == ND_ERR))
+        RInt(ND_ERR);
+
+    if (unlikely((nd_check_fpath(vmsgcomm->comm._name)) == ND_ERR))
+        RInt(ND_ERR);
+    
+    if (unlikely((nd_check_fpath(vmsgcomm->msg.name)) == ND_ERR))
+        RInt(ND_ERR);
+    
+    vmsgcomm->comm.ring = ring_create (
+        vmsgcomm->comm.name, 
+        (uintptr_t)(vmsgcomm->comm.baseaddr), 
+        vmsgcomm->comm.count, 0
+    );
+
+    if (unlikely((!(vmsgcomm->comm.ring)))) {
+        T("errmsg: vmsgcomm->comm.ring: %p", vmsgcomm->comm.ring);
+        RInt(ND_ERR);
+    }
+
+    vmsgcomm->comm._ring = ring_create (
+        vmsgcomm->comm._name, 
+        (uintptr_t)(vmsgcomm->comm._baseaddr), 
+        vmsgcomm->comm.count, 0
+    );
+
+    if (unlikely((!(vmsgcomm->comm._ring)))) {
+        T("errmsg: vmsgcomm->comm.ring: %p", vmsgcomm->comm.ring);
+        RInt(ND_ERR);
+    }
+    
+    vmsgcomm->msg.memory = nd_called_open_mmap_openup_memory (
+        vmsgcomm->msg.name,
+        vmsgcomm->msg.baseaddr,
+        vmsgcomm->msg.memspace,
+        vmsgcomm->comm.count
+    );
+
+    if (unlikely((!(vmsgcomm->msg.memory)))) {
+        T("errmsg: vmsgcomm->comm.ring: %p", vmsgcomm->comm.ring);
+        RInt(ND_ERR);
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief 
+ *  Destroy msgcomm_t structure members
+ * @param vmsgcomm
+ *  msgcomm_t structure pointer
+ * @return
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+static int msgcomm_ending_msgcomm(msgcomm_t * vmsgcomm) {
+
+    TC("Called { %s(%p)", __func__, vmsgcomm);
+
+    if (unlikely((!(vmsgcomm->comm.ring)))) {
+        T("errmsg: vmsgcomm->comm.ring is null");
+    }
+    ring_free(vmsgcomm->comm.ring);
+    unlink(vmsgcomm->comm.name);
+
+    if (unlikely((!(vmsgcomm->comm._ring)))) {
+        T("errmsg: vmsgcomm->comm._ring is null");
+    }
+    ring_free(vmsgcomm->comm._ring);
+    unlink(vmsgcomm->comm._name);
+
+    if (unlikely((!(vmsgcomm->msg.memory)))) {
+        T("errmsg: vmsgcomm->msg.memory is null");
+    }
+    ring_free(vmsgcomm->msg.memory);
+    unlink(vmsgcomm->msg.name);
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief 
+ *  Enqueue the message memory space into the message communication queue
+ * @param vmsgcomm
+ *  msgcomm_t structure pointer
+ * @return
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+static int msgcomm_enq_ring(msgcomm_t * vmsgcomm) {
+
+    TC("Called { %s(%p)", __func__, vmsgcomm);
+
+    ring_t * ring = (ring_t *)(vmsgcomm->comm._ring);
+
+    int i = 0;
+    for (i = 0; i < vmsgcomm->comm.count; i++) {
+        if (unlikely((ring_enqueue(ring, (void *)(vmsgcomm->msg.memory + (i * vmsgcomm->msg.memspace))) != 0))) {
+            T("called ring_enqueue failed");
+            RInt(ND_ERR);
+        }
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief
+ *  Filling Address
+ * @param dir
+ *  Message direction
+ * @param ring
+ *  The secondary pointer that needs to be filled
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+static int msgcomm_filladdr(unsigned int dir, ring_t **ring) {
+
+    TC("Called { %s(%u, %p)", __func__, dir, ring);
+
+    if (unlikely(!ring)) {
+        T("errmsg: ring is null");
+        RInt(ND_ERR);
+    }
+
+    switch (dir) {
+        case MSGCOMM_DIR_0TO1:
+            *ring = msgcomm[0].comm.ring;
+            break;
+        case MSGCOMM_DIR_1TO0:
+            *ring = msgcomm[1].comm.ring;
+            break;
+        default:
+            T("errmsg: msg dir error; dir: %u", dir);
+            RInt(ND_ERR);
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief 
+ *  Message communication resource initialization and startup
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int msgcomm_startup(void) {
+
+    TC("Called { %s()", __func__);
+
+    int i = 0;
+    for (i = 0; i < (sizeof(msgcomm) / sizeof(msgcomm_t)); i++) {
+
+        if (unlikely(((msgcomm_init_msgcomm(&(msgcomm[i]))) == ND_ERR)))
+            RInt(ND_ERR);
+
+        if (unlikely(((msgcomm_enq_ring(&(msgcomm[i]))) == ND_ERR)))
+            RInt(ND_ERR);
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief 
+ *  Message communication resource destruction and exit
+ */
+void msgcomm_ending(void) {
+
+    TC("Called { %s()", __func__);
+
+    int i = 0;
+    for (i = 0; i < (sizeof(msgcomm) / sizeof(msgcomm_t)); i++) {
+        msgcomm_ending_msgcomm(&(msgcomm[i]));
+    }
+
+    RVoid();
+}
+
+
+/**
+ * @brief 
+ *  Message communication module sends messages
+ * @param dir
+ *  Message direction
+ * @param msgtype
+ *  Message Type
+ * @param msg
+ *  Message content
+ * @param length
+ *  Message length
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int msgcomm_sendmsg(unsigned int dir, unsigned int msgtype, const char * msg, int length) {
+
+    TC("Called { %s(%u, %u, %s)", __func__, dir, msgtype, msg);
+
+    if (unlikely((!msg))) {
+        T("errmsg: msg is null");
+    }
+
+    ring_t * _ring = NULL, * ring = NULL;
+
+    if (unlikely(msgcomm_filladdr(dir, &ring) == ND_ERR)) {
+        T("errmsg: called msgcomm_filladdr failed");
+        RInt(ND_ERR);
+    }
+
+    if (unlikely(msgcomm_filladdr(dir, &_ring) == ND_ERR)) {
+        T("errmsg: called msgcomm_filladdr failed");
+        RInt(ND_ERR);
+    }
+
+    if (unlikely((!ring) || (!_ring))) {
+        T("errmsg: ring: %p; _ring: %p", ring, _ring);
+        RInt(ND_ERR);
+    }
+
+    void * obj = NULL;
+    if (unlikely(ring_dequeue(_ring, &obj) != 0)) {
+        T("errmsg: called ring_dequeue failed");
+        RInt(ND_ERR);
+    }
+
+    if (unlikely(!obj)) {
+        T("errmsg: obj is null");
+        RInt(ND_ERR);
+    }
+
+    message_t * message = (message_t *)(obj);
+    message->dir = dir;
+    message->msgtype = msgtype;
+    message->length = length;
+    memcpy(message->msg, msg, length);
+    
+    if (unlikely((ring_enqueue(ring, obj) != 0))) {
+        T("errmsg: called ring_enqueue failed");
+        memset(obj, 0, MSGCOMM_MEMORY_SPACE);
+        ring_enqueue(_ring, obj);
+        RInt(ND_ERR);
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief 
+ *  Message communication module receives messages
+ * @param 
+ *  Message direction
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int msgcomm_recvmsg(unsigned int dir, message_t * message) {
+
+    TC("Called { %s(%u, %p)", __func__, dir, message);
+
+    if (unlikely((!message))) {
+        T("errmsg: message is null");
+        RInt(ND_ERR);
+    }
+
+    ring_t * _ring = NULL, * ring = NULL;
+
+    if (unlikely(msgcomm_filladdr(dir, &ring) == ND_ERR)) {
+        T("errmsg: called msgcomm_filladdr failed");
+        RInt(ND_ERR);
+    }
+
+    if (unlikely(msgcomm_filladdr(dir, &_ring) == ND_ERR)) {
+        T("errmsg: called msgcomm_filladdr failed");
+        RInt(ND_ERR);
+    }
+
+    if (unlikely((!ring) || (!_ring))) {
+        T("errmsg: ring: %p; _ring: %p", ring, _ring);
+        RInt(ND_ERR);
+    }
+
+    unsigned int nums = ring_count(ring);
+    if (nums <= 0 || nums > MSGCOMM_BLOCK_NUMBERS) {
+        T("errmsg: the number of elements in the queue: %u", nums);
+        RInt(ND_ERR);
+    }
+
+    void * obj = NULL;
+    if (unlikely((ring_dequeue(ring, &obj) != 0))) {
+        T("errmsg: called ring_dequeue failed");
+        RInt(ND_ERR);
+    }
+
+    message_t * tmp = (message_t *)(obj);
+
+    message->dir = tmp->dir;
+    message->msgtype = tmp->msgtype;
+    message->length = tmp->length;
+    memcpy(message->msg, tmp->msg, tmp->length);
+
+    if (unlikely((ring_enqueue(_ring, obj) != 0))) {
+        T("errmsg: called ring_enqueue failed");
+    }
+
+    RInt(ND_OK);
+}
+
+
+/**
+ * @brief
+ *  Check whether there is a message in the specified direction
+ * @param dir
+ *  Message direction
+ * @return
+ *  If it exists, it returns the number of messages.
+ */
+int msgcomm_detection(unsigned int dir) {
+
+    TC("Called { %s(%u)", __func__, dir);
+
+    ring_t * ring = NULL;
+
+    if (unlikely(msgcomm_filladdr(dir, &ring) == ND_ERR)) {
+        T("errmsg: called msgcomm_filladdr failed");
+        RInt(0);
+    }
+
+    if (unlikely((!ring))) {
+        T("errmsg: ring: %p; _ring: %p", ring);
+        RInt(0);
+    }
+
+    unsigned int nums = ring_count(ring);
+
+    RInt(nums);
+}
--- msgcomm.h
+++ msgcomm.h
@@ -14,6 +14,171 @@
 #ifndef __MSGCOMM_H__
 #define __MSGCOMM_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "common.h"
+#include "ring.h"
+
+
+/**
+ * @brief 
+ *  Structure for message communication
+ * @memberof comm
+ *  Common structures for communication
+ * @memberof msg.memery 
+ *  The starting address of the memory space
+ * @memberof msg.baseaddr
+ *  Initialize the base address of the memory space
+ * @memberof msg.dir
+ *  Direction of message communication
+ * @memberof msg.memspace 
+ *  The size of the memory space for each element
+ * @memberof msg.name 
+ *  The file name corresponding to memory
+ */
+typedef struct {
+
+	comm_t comm;
+	
+	struct {
+		void * memory;
+		void * baseaddr;
+        unsigned int dir;
+		unsigned int memspace;
+		char name[COMM_NAMESIZE];
+	} msg;
+	
+} msgcomm_t;
+
+
+/**
+ * @brief 
+ *  The format used for message communication
+ * @memberof dir
+ *  Direction of message communication
+ * @memberof msgtype
+ *  Types of message communication
+ * @memberof msg
+ *  Message content
+ */
+typedef struct {
+	
+	unsigned int dir;
+	unsigned int msgtype;
+	unsigned int length;
+	unsigned char msg[0];
+
+} message_t;
+
+
+/**
+ * @brief 
+ *  Communication direction display --> capture 0TO1
+ */
+#define MSGCOMM_DIR_0TO1        0x01U
+/**
+ * @brief 
+ *  Communication direction capture --> display 1TO0
+ */
+#define MSGCOMM_DIR_1TO0        0x10U
+
+
+/**
+ * @brief
+ *  Define the type of communication
+ * @memberof MSGCOMM_SUC
+ *  Message processed successfully
+ * @memberof MSGCOMM_ACK
+ *  Response after receiving the message
+ * @memberof MSGCOMM_ERR
+ *  Message processing error
+ * @memberof MSGCOMM_CMD
+ *  Command type message
+ */
+enum {
+    MSGCOMM_SUC = 0XF0U,
+    MSGCOMM_ACK = 0xF1U,
+    MSGCOMM_ERR = 0XF2U,
+    MSGCOMM_CMD = 0xF3U,
+};
+
+
+/**
+ * @brief 
+ *  The base address used for message communication
+ * @memberof MSGCOMM_BASE_ADDR_RING0TO1
+ *  ring 0TO1 base address
+ * @memberof MSGCOMM_BASE_ADDR__RING0TO1
+ *  _ring 0TO1 base address
+ * @memberof MSGCOMM_BASE_ADDR_MEM0TO1
+ *  memery 0TO1 base address
+ * @memberof MSGCOMM_BASE_ADDR_RING1TO0
+ *  ring 1TO0 base address
+ * @memberof MSGCOMM_BASE_ADDR__RING1TO0
+ *  _ring 1TO0 base address
+ * @memberof MSGCOMM_BASE_ADDR_MEM1TO0
+ *  memery 1TO0 base address
+ */
+#define MSGCOMM_BASE_ADDR_RING0TO1              (void *)(0x6EE000000000)
+#define MSGCOMM_BASE_ADDR__RING0TO1             (void *)(0x6EE001048510)
+#define MSGCOMM_BASE_ADDR_MEM0TO1               (void *)(0x6EE002090A20)
+#define MSGCOMM_BASE_ADDR_RING1TO0              (void *)(0x6EE0030D8F30)
+#define MSGCOMM_BASE_ADDR__RING1TO0             (void *)(0x6EE004121440)
+#define MSGCOMM_BASE_ADDR_MEM1TO0               (void *)(0x6EE005169950)
+
+
+/**
+ * @brief 
+ * 	Number of memory block elements and memory block size
+ * @memberof MSGCOMM_BLOCK_NUMBERS
+ * 	Number of memory block elements
+ */
+#define MSGCOMM_BLOCK_NUMBERS					8
+#define MSGCOMM_MEMORY_SPACE					1024
+
+
+/**
+ * @brief 
+ *  The file name used for message communication
+ * @memberof MSGCOMM_RING_FNAME_0TO1
+ *  ring 0TO1 file name
+ * @memberof MSGCOMM__RING_FNAME_0TO1
+ *  _ring 0TO1 file name
+ * @memberof MSGCOMM_MEM_FNAME_0TO1
+ *  memery 0TO1 file name
+ * @memberof MSGCOMM_RING_FNAME_1TO0
+ *  ring 1TO0 file name
+ * @memberof MSGCOMM__RING_FNAME_1TO0
+ *  _ring 1TO0 file name
+ * @memberof MSGCOMM_MEM_FNAME_1TO0
+ *  memory 1TO0 file name
+ */
+#define MSGCOMM_RING_FNAME_0TO1                 "/dev/shm/.Ring0TO1"
+#define MSGCOMM__RING_FNAME_0TO1                "/dev/shm/._Ring0TO1"
+#define MSGCOMM_MEM_FNAME_0TO1                  "/dev/shm/.Mem0TO1"
+#define MSGCOMM_RING_FNAME_1TO0                 "/dev/shm/.Ring1TO0"
+#define MSGCOMM__RING_FNAME_1TO0                "/dev/shm/._Ring1TO0"
+#define MSGCOMM_MEM_FNAME_1TO0                  "/dev/shm/.Mem1TO0"
+
+
+/**
+ * @brief 
+ *  Message communication resource initialization and startup
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int msgcomm_startup(void);
+
+
+/**
+ * @brief 
+ *  Message communication resource destruction and exit
+ */
+void msgcomm_ending(void);
 
 
 #endif  // __MSGCOMM_H__
