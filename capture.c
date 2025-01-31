7521a8e37194b6888c606a0b25f33e9d51d38bed
nothing <nothing163mail@163.com>
2025-02-01 01:17:56 +0800

    [add] add capture_main function

--- capture.c
+++ capture.c
@@ -10,3 +10,32 @@
  * 
  * 
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "capture.h"
+#include "common.h"
+#include "trace.h"
+
+
+/**
+ * @brief 
+ *  The main function of the packet capture function
+ * @param COREID
+ *  COREID corresponding to the packet capture process
+ * @param pname
+ *  The name of the packet capture process
+ * @param param
+ *  Retain Parameters
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int capture_main (unsigned int COREID, const char * pname, void * param) {
+
+    
+
+    RInt(ND_OK);
+}
--- capture.h
+++ capture.h
@@ -14,6 +14,26 @@
 #ifndef __CAPTURE_H__
 #define __CAPTURE_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "common.h"
+#include "trace.h"
 
+/**
+ * @brief 
+ *  The main function of the packet capture function
+ * @param COREID
+ *  COREID corresponding to the packet capture process
+ * @param pname
+ *  The name of the packet capture process
+ * @param param
+ *  Retain Parameters
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int capture_main (unsigned int COREID, const char * pname, void * param);
 
 #endif  // __CAPTURE_H__
