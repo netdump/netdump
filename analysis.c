48cb4bfeefc502aa55f398fae0a1ff86dc876f37
nothing <nothing163mail@163.com>
2025-02-01 01:17:22 +0800

    [add] add analysis_main function

--- analysis.c
+++ analysis.c
@@ -10,3 +10,31 @@
  * 
  * 
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "analysis.h"
+#include "common.h"
+#include "trace.h"
+
+
+/**
+ * @brief 
+ *  The main function of the packet parsing process
+ * @param COREID
+ *  COREID corresponding to the packet capture process
+ * @param pname
+ *  The name of the packet capture process
+ * @param param
+ *  Retain Parameters
+ * @return 
+ *  If successful, it returns ND_OK; 
+ *  if failed, it returns ND_ERR
+ */
+int analysis_main (unsigned int COREID, const char * pname, void * param) {
+
+
+    RInt(ND_OK);
+}
