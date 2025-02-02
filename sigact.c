13f0638c10849059e289af3272c807ad285dd163
nothing <nothing163mail@163.com>
2025-02-02 21:37:57 +0800

    [mod] modify logging

--- sigact.c
+++ sigact.c
@@ -148,7 +148,7 @@ void sigact_handle_crash (int signum) {
 
 	unsigned int lCOREID = lcore_id();
 
-	T("infomsg: lCOREID: %u", lCOREID);
+	TI("lCOREID: %u", lCOREID);
 
 	if (GCOREID_DP == lCOREID) {
 		display_exit_resource_destruction();
@@ -175,7 +175,7 @@ void sigact_handle_quit (int signum) {
 
 	unsigned int lCOREID = lcore_id();
 
-	T("infomsg: lCOREID: %u", lCOREID);
+	TI("lCOREID: %u", lCOREID);
 
 	if (GCOREID_DP == lCOREID) {
 		display_exit_resource_destruction();
@@ -232,17 +232,17 @@ void sigact_handle_child_quit (int signum) {
 	if (pid > 0) {
 		if (WIFEXITED(status)) {
 
-			T("infomsg: Child process %d exited with status %d", pid, WEXITSTATUS(status));
+			TI("Child process %d exited with status %d", pid, WEXITSTATUS(status));
 			sigact_general_code (pid);
 
 		} else if (WIFSIGNALED(status)) {
 
-			T("infomsg: Child process %d exited due to signal %d", pid, WTERMSIG(status));
+			TI("Child process %d exited due to signal %d", pid, WTERMSIG(status));
 			sigact_general_code (pid);
 
 		} else {
 
-			T("infomsg: Child process %d exited abnormally", pid);
+			TI("Child process %d exited abnormally", pid);
 			sigact_general_code (pid);
 			
 		}
