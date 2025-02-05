91fc360fd0c1ae94bc4839a7165eb3380d4da152
nothing <nothing163mail@163.com>
2025-02-05 18:45:19 +0800

    [add] add function strlcpy(char *dst, const char *src, size_t siz)

--- common.h
+++ common.h
@@ -305,4 +305,24 @@ void * nd_called_mmap_lookup_memory (
  */
 void nd_delay_microsecond (unsigned int sec, unsigned long microseconds);
 
+
+/*
+ * Copy src to string dst of size siz.  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+/**
+ * @brief
+ *  Copy src to string dst of size siz.
+ * @param dst
+ *  dst addr
+ * @param src
+ *  src addr
+ * @param siz
+ *  length
+ * @return
+ *  copy length
+ */
+size_t strlcpy(char *dst, const char *src, size_t siz);
+
 #endif 
