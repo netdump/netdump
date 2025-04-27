3aaa94d524c01d9eb0c78d7ca8180f87771f6fe6
nothing <nothing163mail@163.com>
2025-04-27 18:26:18 +0800

    [add] add min macro

--- common.h
+++ common.h
@@ -115,7 +115,7 @@ typedef struct nd_dll_s
  * @brief
  * 	Get the minimum value
  */
-#define min(x, y) ({ typeof(x) _x = (x); typeof(y) _y = (y); _x < _y ? _x : _y; })
+#define MINIMUM(x, y) ({ typeof(x) _x = (x); typeof(y) _y = (y); _x < _y ? _x : _y; })
 
 
 /**
