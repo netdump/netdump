0a2849d56ec438c701ddebd67737676b7959416c
nothing <nothing163mail@163.com>
2025-04-30 17:37:27 +0800

    [add] add function

---
+++ a-f-atm.c
@@ -0,0 +1,16 @@
+
+
+#include "header.h"
+
+
+/*
+ * This is the top level routine of the printer.  'p' points
+ * to the LLC/SNAP header of the packet, 'h->ts' is the timestamp,
+ * 'h->len' is the length of the packet off the wire, and 'h->caplen'
+ * is the number of bytes actually captured.
+ */
+void atm_if_print(void * infonode, const struct pcap_pkthdr *h, const u_char *p)
+{
+
+    return ;
+}
