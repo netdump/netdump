4fb36ea01f3a97bd762bd25b3a6f41d4a40de8ec
nothing <nothing163mail@163.com>
2025-04-30 17:09:11 +0800

    [add] add header.h file

---
+++ header.h
@@ -0,0 +1,26 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#ifndef __HEADER_H__
+#define __HEADER_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+
+typedef unsigned char u_char;
+
+#endif  // __HEADER_H__
