8b8443826b4c20b7cf6103786a969bbb336eca2a
nothing <nothing163mail@163.com>
2025-04-30 20:17:49 +0800

    [add] add some macro

--- header.h
+++ header.h
@@ -23,4 +23,33 @@
 
 typedef unsigned char u_char;
 
+/*
+ * Check whether this is GCC major.minor or a later release, or some
+ * compiler that claims to be "just like GCC" of that version or a
+ * later release.
+ */
+
+#if !defined(__GNUC__)
+/* Not GCC and not "just like GCC" */
+#define ND_IS_AT_LEAST_GNUC_VERSION(major, minor) 0
+#else
+/* GCC or "just like GCC" */
+#define ND_IS_AT_LEAST_GNUC_VERSION(major, minor) \
+    (__GNUC__ > (major) ||                        \
+     (__GNUC__ == (major) && __GNUC_MINOR__ >= (minor)))
+#endif
+
+#if __has_attribute(unused) || ND_IS_AT_LEAST_GNUC_VERSION(2, 0)
+/*
+ * Compiler with support for __attribute__((unused)), or GCC 2.0 and
+ * later, so it supports __attribute__((unused)).
+ */
+#define _U_ __attribute__((unused))
+#else
+/*
+ * We don't know of any way to mark a variable as unused.
+ */
+#define _U_
+#endif
+
 #endif  // __HEADER_H__
