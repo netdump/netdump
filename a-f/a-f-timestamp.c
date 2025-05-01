a16615f35efdfc9ee3f080048d26c9f58ddcc03e
nothing <nothing163mail@163.com>
2025-05-01 22:39:49 +0800

    [add] add timestamp handle

--- a-f/a-f-timestamp.c
+++ a-f/a-f-timestamp.c
@@ -3,11 +3,58 @@
 #include "header.h"
 
 
-void analysis_ts_print(ndo_t *ndo, const struct timeval * tv, char * timestamp) 
+#define INVALIDTIMESTAMP        "00:00:00.000000"
+
+
+/*
+ * Print the timestamp as HH:MM:SS.FRAC.
+ */
+static void ts_hmsfrac_print(ndo_t *ndo, const struct timeval *tv, char * timestamp)
+{
+
+    TI("Called { %s(%p, %p, %p)}", __func__, ndo, tv, timestamp);
+
+    struct tm *tm = tm = localtime(&tv->tv_sec);
+
+    if (!tm) 
+    {
+        memcpy(timestamp, INVALIDTIMESTAMP, strlen(INVALIDTIMESTAMP));
+        RVoid();
+    }
+
+    if (strftime(timestamp, strlen(INVALIDTIMESTAMP), "%H:%M:%S", tm) == 0)
+    {
+        memcpy(timestamp, INVALIDTIMESTAMP, strlen(INVALIDTIMESTAMP));
+        RVoid();
+    }
+
+    sprintf((timestamp + strlen(timestamp)), ".%06u", (unsigned)tv->tv_usec);
+
+    RVoid();
+}
+
+
+/**
+ * @brief
+ *  
+ */
+void analysis_ts_print(ndo_t * ndo, const struct timeval *tv, char *timestamp)
 {
     TC("Called { %s (%p, %p, %p)", __func__, ndo, tv, timestamp);
 
-    
+    if (!ndo || !tv || !timestamp)
+    {
+        memcpy(timestamp, INVALIDTIMESTAMP, strlen(INVALIDTIMESTAMP));
+        RVoid();
+    }
+
+    if (tv->tv_sec < 0)
+    {
+        memcpy(timestamp, INVALIDTIMESTAMP, strlen(INVALIDTIMESTAMP));
+        RVoid();
+    }
+
+    ts_hmsfrac_print(ndo, tv, timestamp);
 
     RVoid();
 }
