cac7e72fae9e4fec4e010031ad5fb26721b2a0d3
nothing <nothing163mail@163.com>
2025-05-01 20:20:29 +0800

    [add] add file

---
+++ a-f/a-f-timestamp.c
@@ -0,0 +1,13 @@
+
+
+#include "header.h"
+
+
+void analysis_ts_print(ndo_t *ndo, const struct timeval * tv, char * timestamp) 
+{
+    TC("Called { %s (%p, %p, %p)", __func__, ndo, tv, timestamp);
+
+    
+
+    RVoid();
+}
