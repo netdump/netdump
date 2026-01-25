9cc7cf8b41005ebd68d3d28bfdd76942a2f2a5f5
nothing <nothing163mail@163.com>
2026-01-25 16:21:03 +0800

    [mod] del funcscope function

--- capture.c
+++ capture.c
@@ -14,7 +14,6 @@
 #include "capture.h"
 #include "d2c_comm.h"
 
-#include "funcscope.h"
 
 /**
  * @brief rfmon (monitor) mode
@@ -173,8 +172,6 @@ int capture_main (unsigned int COREID, const char * pname, void * param) {
         goto label1;
     }
 
-    funcscope_caller_initialize(1, FS_LITE);
-
     if (unlikely((capture_loop()) == ND_ERR)) {
         TE("Analysis loop startup failed");
         goto label1;
@@ -218,8 +215,6 @@ int capture_loop (void) {
         TI("message.msgtype: %u", message.msgtype);
         TI("message.length: %u", message.length);
         TI("message.content: %s", message.content);
-
-        funcscope_server_poll_and_send_fd();
 
         capture_parsing_cmd_and_exec_capture((char *)(message.content));
     }
@@ -1821,9 +1816,6 @@ int capture_parsing_cmd_and_exec_capture(char * command)
 
     while (1)
     {
-
-        FUNCSCOPE_ENTER(0);
-
         unsigned int tmp = 0;
         msgcomm_receive_status_value(&(d2c_flag_statistical.d2c_run_flag_val), tmp);
 
@@ -1869,8 +1861,6 @@ int capture_parsing_cmd_and_exec_capture(char * command)
             __atomic_store_n(&(d2c_flag_statistical.packages), state_value.cur_pkts_sn, __ATOMIC_RELEASE);
             state_value.last_tick = now_tick;
         }
-
-        FUNCSCOPE_EXIT(0);
     }
 
     pcap_close(pd);
